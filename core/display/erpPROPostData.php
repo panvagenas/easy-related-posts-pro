7539165318908b04b162d2ebb87cf3d7bca2e409
vagenas <pan.vagenas@gmail.com>
2014-07-20 17:39:15 +0300

    Fixed notice produced by undefind index in erpPostData getThumbnail

--- core/display/erpPROPostData.php
+++ core/display/erpPROPostData.php
@@ -315,16 +315,18 @@ class erpPROPostData {
      * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
      * @since 1.0.0
      */
-    public function setThumbnail($defaultThumbnail, $size = 'single-post-thumbnail') {        
+	public function setThumbnail($defaultThumbnail, $size = 'single-post-thumbnail') {        
         if (has_post_thumbnail($this->ID)) {
         	$image_url = wp_get_attachment_image_src(get_post_thumbnail_id($this->ID), $size);
         	$image_url = $image_url[0];
         } else {
         	$image_url = '';
         	$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $this->post->post_content, $matches);
-        	$image_url = $matches [1] [0];
-        	if(empty($image_url)){
+        	
+        	if(empty($matches [1] [0])){
         		$image_url = $defaultThumbnail;
+        	} else {
+        		$image_url = $matches [1] [0];
         	}
         }
         $this->thumbnail = $image_url;
